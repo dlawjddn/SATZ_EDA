e2d25e84d62351d3b00d084ecf69ddf38c550242
Lim jeong woo <dlawjddn0105@gmail.com>
2025-01-18 19:45:42 +0900

    FIX: 동기 이벤트로 변경, 아무리 생각해도 이게 맞는거 같음

--- src/main/java/satz/event/satzeda/tradehistory/event/listener/TradeHistoryListener.java
+++ src/main/java/satz/event/satzeda/tradehistory/event/listener/TradeHistoryListener.java
@@ -2,9 +2,11 @@ package satz.event.satzeda.tradehistory.event.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import satz.event.satzeda.tradehistory.domain.TradeHistory;
 import satz.event.satzeda.tradehistory.domain.TradeType;
 import satz.event.satzeda.tradehistory.repository.TradeHistoryRepository;
@@ -18,9 +20,8 @@ public class TradeHistoryListener {
     public TradeHistoryListener(TradeHistoryRepository tradeHistoryRepository) {
         this.tradeHistoryRepository = tradeHistoryRepository;
     }
-
-    @Async
-    @EventListener
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void deposit(DepositEvent depositEvent) {
         log.info("입금발생!!! " + depositEvent.getAmount().toString());
 
