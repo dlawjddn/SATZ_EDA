db6539c18403922d46ec977c9c52b949aaaee1be
Lim jeong woo <dlawjddn0105@gmail.com>
2025-01-18 14:33:09 +0900

    first commit

---
+++ src/main/java/satz/event/satzeda/SatzEdaApplication.java
@@ -0,0 +1,13 @@
+package satz.event.satzeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatzEdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatzEdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/satz/event/satzeda/SatzEdaApplicationTests.java
@@ -0,0 +1,13 @@
+package satz.event.satzeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatzEdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
